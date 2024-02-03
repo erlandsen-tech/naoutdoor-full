81a2fa8131046ae0b2e21dfec75ae5d9ffd8c76f
John Inge Muldal Erlandsen <john.inge.muldal.erlandsen@dnb.no>
2024-02-03 20:56:29 +0100

    remove amplify

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css'
-import App from './App'
-import {Helmet} from 'react-helmet';
-import {Amplify} from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(amplifyconfig);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
+import App from "./App";
+import { Helmet } from "react-helmet";
 
 const TITLE = "Ski & Recovery";
 
-
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<div>
+root.render(
+  <div>
     <Helmet>
-        <title>{TITLE}</title>
+      <title>{TITLE}</title>
     </Helmet>
-    <App/>
-</div>);
+    <App />
+  </div>
+);

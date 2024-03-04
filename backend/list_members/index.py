010a1ea9d48f037ae6f18a04c8c6043cbccd44ec
John Inge Muldal Erlandsen <john.inge.muldal.erlandsen@dnb.no>
2024-03-04 09:48:14 +0100

    potentialy fix list members

--- backend/list_members/index.py
+++ backend/list_members/index.py
@@ -1,17 +1,29 @@
+import json
+
 import boto3
 from datetime import datetime
 
 
 def lambda_handler(event, context):
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("member")
+    try:
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table("member")
+        response = table.scan()
+        items = response["Items"]
 
-    response = table.scan()
-    items = response["Items"]
+        for item in items:
+            clean_date = datetime.strptime(item["CleanDate"], "%d-%m-%Y")
+            days_since_clean = (datetime.now() - clean_date).days
+            item["DaysSinceClean"] = days_since_clean
 
-    for item in items:
-        clean_date = datetime.strptime(item["CleanDate"], "%d-%m-%Y")
-        days_since_clean = (datetime.now() - clean_date).days
-        item["DaysSinceClean"] = days_since_clean
-
-    return items
+        return {
+            'statusCode': 200,
+            'headers': {"Content-Type": "application/json"},
+            'body': json.dumps(items)
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {"Content-Type": "application/json"},
+            'body': {'exception': json.dumps(str(e))}
+        }

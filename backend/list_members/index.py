d9695dde47c338ec8d0a415d1391da7079e14df4
John Inge Muldal Erlandsen <john.inge.muldal.erlandsen@dnb.no>
2024-03-06 12:58:34 +0100

    add members

--- backend/list_members/index.py
+++ backend/list_members/index.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 import boto3
 from datetime import datetime
@@ -11,10 +12,23 @@ def lambda_handler(event, context):
         response = table.scan()
         items = response["Items"]
 
+        total_days = 0
+        members_total = len(items)
+        country_stats = {}
+
         for item in items:
             clean_date = datetime.strptime(item["CleanDate"], "%d-%m-%Y")
             days_since_clean = (datetime.now() - clean_date).days
             item["DaysSinceClean"] = days_since_clean
+            if item["Country"] not in country_stats:
+                country_stats["Country"] = {
+                    "total_days": days_since_clean,
+                    "member_count": 1,
+                }
+            else:
+                country_stats["Country"]["total_days"] += days_since_clean
+                country_stats["Country"]["member_count"] += 1
+            total_days += days_since_clean
 
         return {
             'statusCode': 200,
@@ -23,7 +37,11 @@ def lambda_handler(event, context):
                 "Access-Control-Allow-Origin": "http://localhost:3000",
                 "Access-Control-Allow-Methods": "POST,OPTIONS,GET",
                 "Content-Type": "application/json"
-            },            'body': json.dumps(items)
+            }, 'body': {
+                "total_days": total_days,
+                "country_stats": country_stats,
+                "members_total": members_total,
+            }
         }
     except Exception as e:
         return {
